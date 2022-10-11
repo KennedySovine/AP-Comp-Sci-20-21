c352f14277ef1a01163046404d1b6b9a120b8019
KennedySovine <89476714+KennedySovine@users.noreply.github.com>
2022-10-11 15:07:24 +0100

    Add files via upload

---
+++ Unit 7/Notes/2dArrays.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class 2dArrays {
+   public static void main (String [] args){
+   /*
+   type[] name = new type[length]
+   
+   
+   2-D arrays are arrays that have both rows and columns.
+    -A row has horizontal elements.
+    -A column has vertical elements
+    
+   2D arrays are useful when the data is naturally organized in rows and columns like in a
+   spreadsheet, bingo, battleship, etc.
+   
+   Java stores 2D arrays as arrays of arrays. Each element of the outer array has a reference to
+   each inner array.
+   ROW MAJOR ORDER
+   When you talk about a specific element in an array, you list the row first and then the column.
+   
+   int [][] scores = new int [num rows][num cols];
+   int [][] scores = {{57, 50, 2}, {40, 70, 35}};
+   
+   In order to traverse 2D arrays, you have to use nested loops.
+   
+   for (int row=0; row<array.length; row++){
+      for (int col=0; col<array[0].length; col++){
+         System.out.println (array[row][col]);
+      }
+   }
+   */
+   }
+}
